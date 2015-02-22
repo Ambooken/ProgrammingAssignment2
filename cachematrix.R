f6767528d6ba377bc9d81cf77df68c220acdf9b4
Ambooken <sxtho33@gmail.com>
2015-02-22 10:29:59 -0500

    second commit after indenting

--- cachematrix.R
+++ cachematrix.R
@@ -3,17 +3,17 @@
 ## 
 
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setinverse <- function(solve) m <<- solve
-  getmean <- function() m
-  list(set = set, get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) m <<- solve
+    getmean <- function() m
+    list(set = set, get = get,
+          setinverse = setinverse,
+          getinverse = getinverse)
 
 }
 
@@ -21,14 +21,14 @@ makeCacheMatrix <- function(x = matrix()) {
 ## This function will cache the inverse created on feb-21-2015
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-  m <- x$getinverse()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  data <- x$get()
-  m <- solve(data, ...)
-  x$setinverse(m)
-  m
+  ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
 }
